fa5482d131709cd7609ff19c3cbe7fc706aedf5c
Berqia Mouad <83679093+BerqiaMouad@users.noreply.github.com>
2021-10-25 12:40:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    //menu navbar apearence
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+
+
+    //owl-carousel script
+    $('.team_members').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplaytimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
