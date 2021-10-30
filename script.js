40b2955489464c4a4d28166301c2fb5617742e02
Berqia Mouad <83679093+BerqiaMouad@users.noreply.github.com>
2021-10-30 18:41:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,4 +37,12 @@ $(document).ready(function(){
             }
         }
     });
+
+    //scroll bar effect
+    let progress = document.getElementById('progressbar');
+    let totalHeight = document.body.scrollHeight - window.innerHeight;
+    window.onscroll = function() {
+        let progressHeight = (window.pageYOffset / totalHeight) * 100;
+        progress.style.height = progressHeight + "%";
+    }
 });
